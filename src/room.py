fdb38a37b8e7d62555d37636e6d2997e77cf93d5
Mohd Raza <noorinfotechinc@gmail.com>
2020-09-12 22:31:30 -0500

    working

--- src/room.py
+++ src/room.py
@@ -2,13 +2,22 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, list):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.list = list
+
+    def printItems(self):
+        output = f"{self.name} has\n"
+        i = 0
+        for item in self.list:
+            output += " " + str(i+1) + ". " + item + "\n"
+        # Add line below
+        return output
 
     def __str__(self):
         return f"Current Room: {self.name} \nDescription: {self.description} "
